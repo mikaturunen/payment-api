8dbb2a069cf164f8040b092b0d6751ad8ee88bc3
Mika Turunen <mika.turunen@ymail.com>
2017-08-10 11:19:27 +0300

    Nonsensical changes.

--- tests/api/controllers/v1/payment.js
+++ tests/api/controllers/v1/payment.js
@@ -28,16 +28,17 @@ describe('Legacy payment wrapper', () => {
     it('with a valid payment', done => {
       let body = require('../../../../src/api/mocks/payment.json')
       // we need to make sure that each test run the stamp is unique
-      body.payment.stamp = body.payment.stamp + new Date().getTime()
+      body.payment.stamp = body.payment.stamp //+ new Date().getTime()
+
+
+      body.hmac = crypto
+        .createHmac('sha256', 'SAIPPUAKAUPPIAS')
+        .update(new Buffer(JSON.stringify(body.payment)).toString('base64'))
+        .digest('hex')
+        .toUpperCase()
 
       console.log('body to post')
       console.log(JSON.stringify(body, null,2 ))
-
-      body.hmac = crypto
-       .createHmac('sha256', 'SAIPPUAKAUPPIAS')
-       .update(new Buffer(JSON.stringify(body.payment)).toString('base64'))
-       .digest('hex')
-       .toUpperCase()
 
       chai
         .request(server)
@@ -45,7 +46,7 @@ describe('Legacy payment wrapper', () => {
         .set('Accepts', 'application/json')
         .send(body)
         .end((error, response) => {
-          console.log(JSON.stringify(response.body, null, 2))
+          console.log('response:', JSON.stringify(response.body, null, 2))
           response.status.should.eql(200)
           response.body.should.be.a('object')
 
